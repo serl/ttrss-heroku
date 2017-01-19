e9b4c1ebea7a429dd25823f84b71245deaac4840
Sergio Livi <livi@i3s.unice.fr>
2017-01-19 15:26:28 +0100

    update icons worker

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 web: ./web-boot.sh
 update: ./update.sh
+update-icons: ./update.sh icons
 update-daemon: ./update.sh daemon
--- update.sh
+++ update.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-if [ "$1" == "daemon" ]; then
-    (while true; do php s3_sync.php upload; sleep 600; done) &
-    php tt-rss/update_daemon2.php
-else
-    php tt-rss/update.php --feeds
-    php s3_sync.php upload
-fi
+case "$1" in
+    daemon)
+        '(while true; do php s3_sync.php upload; sleep 600; done) &'
+        php tt-rss/update_daemon2.php
+        ;;
+    icons)
+        psql "$DATABASE_URL" -c 'UPDATE ttrss_feeds SET favicon_last_checked=NULL, last_updated=NULL, last_update_started=NULL;' 2>/dev/null
+        "$0"
+        echo "Now you should restart the web dyno to actually see the icons."
+        echo "Do this by running `heroku restart`."
+        ;;
+    *)
+        php tt-rss/update.php --feeds
+        php s3_sync.php upload
+        ;;
+esac
